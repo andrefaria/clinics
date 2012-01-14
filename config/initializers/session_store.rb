21bb6be018e9346c528a0e727c7b75382fe9752c
Daniel <danielfg@danielfg-Infoway.(none)>
2012-01-14 16:04:42 -0200

    minor

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-Clinics::Application.config.session_store :cookie_store, key: '_clinics_session'
+# Default config
+# Clinics::Application.config.session_store :cookie_store, key: '_clinics_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Clinics::Application.config.session_store :active_record_store
+Clinics::Application.config.session_store :active_record_store
